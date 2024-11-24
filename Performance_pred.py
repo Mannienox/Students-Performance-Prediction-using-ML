e0eb8a5edc28f769aae099f76bc31e537cec1f12
Mannienox <94338608+Mannienox@users.noreply.github.com>
2024-11-24 14:58:11 +0100

    Update Performance_pred.py

--- Performance_pred.py
+++ Performance_pred.py
@@ -8,11 +8,11 @@ survey_data = []
 survey_form = {
     'Are you in any romantic relationship?': ['0. No', '1. Yes'],
     'How much attendance will you want to have at the end of Real Analysis 1 Class?': ['4: More than 70% of the time', '3: Between 60% and 69% of the time', '2: Between 50% and 59% of the time', '1: Between 40% and 49% of the time', '0: Less than 40% of the time'],
-    'How punctual will you be to the Real Analysis 1 class?': ['0: Always arrive late', '1: Frequently arrive late', '2: Occasionally arrive late', '3: Usually arrive on time', '4: Always arrive on time'],
+    'How punctual will you be to the Real Analysis 1 class?': ['0: Will always arrive late', '1: Will frequently arrive late', '2: Will occasionally arrive late', '3: Will usually arrive on time', '4: Will always arrive on time'],
     'How supportive is your family financially?': ['2: Total support (family covers all educational expenses)', '1: Partial support (family covers some educational expenses)', '0: No financial support from family'],
-    'Do you like your course of study (mathematics/statistics) when you were?' : ['0. No', '1. Yes'],
+    'Do you like your course of study (Mathematics/Statistics)?' : ['0. No', '1. Yes'],
     'Do you like Real Analysis 1?': ['0. No', '1. Yes'],
-    'How well are you understanding Real Analysis 1 so far?': ['2: Totally (l understood everything or almost everything)', '1. Partially (l understood some parts and crammed some)', '0. No Understanding (l crammed everything)'],
+    'How well are you understanding Real Analysis 1 so far?': ['2: Totally (I understood everything or almost everything)', '1. Partially (I understood some parts and crammed some)', '0. No Understanding (l crammed everything)'],
     'What was you grade in the Pre-requisite Course (Sets and Numbers System)?': ['5. A', '4. B', '3. C', '2. D', '1. E', '0. F']}
 
 
